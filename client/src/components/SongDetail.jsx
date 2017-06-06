f04fc8348ca7ee53e6ba27ee1a6a12cb53e18084
Michael Shennan <m.shennan22@gmail.com>
2017-06-06 20:26:02 +0100

    song detail

---
+++ client/src/components/SongDetail.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SongDetail extends React.Component {
+
+  render() {
+    if (this.props.song === null) return null;
+
+    return (
+      <div className="song-details">
+        <p>Title: {this.props.song.im:name.label}</p>
+        <p>Artist: {this.props.song.im:artist.label}</p>
+      </div>
+    )
+  }
+
+}
+
+export default SongDetail;
